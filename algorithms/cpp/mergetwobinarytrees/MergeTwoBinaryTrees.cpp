4cb833645a6ef12755fbdd20e985cbf6b1a8d6b3
madingx <476100112@qq.com>
2017-11-10 23:50:41 +0800

    617合并两个树

---
+++ algorithms/cpp/mergetwobinarytrees/MergeTwoBinaryTrees.cpp
@@ -0,0 +1,54 @@
+// Source : https://oj.leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+// Author : Mading
+// Date   : 2017-11-10
+
+/********************************************************************************** 
+* 617. Merge Two Binary Trees
+* 
+* Given two binary trees and imagine that when you put one of them to cover * the other,
+* some nodes of the two trees are overlapped while the others are not.
+* 
+* You need to merge them into a new binary tree. The merge rule is that if two 
+* nodes overlap, then sum node values up as the new value of the merged node. 
+* Otherwise, the NOT null node will be used as the node of new tree.
+*
+* Example 1:
+*       Tree 1                     Tree 2                  
+*          1                         2                             
+*         / \                       / \                            
+*        3   2                     1   3                        
+*       /                           \   \                      
+*      5                             4   7                  
+* Output: 
+* Merged tree:
+*         3
+*        / \
+*       4   5
+*      / \   \ 
+*     5   4   7
+* 
+**********************************************************************************/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if ( t1 && t2 ) {  
+            TreeNode * root = new TreeNode(t1->val + t2->val);  
+            root->left = mergeTrees(t1->left, t2->left);  
+            root->right = mergeTrees(t1->right, t2->right);  
+            return root;  
+        } else {  
+            return t1 ? t1 : t2;  
+        }  
+    }
+};
