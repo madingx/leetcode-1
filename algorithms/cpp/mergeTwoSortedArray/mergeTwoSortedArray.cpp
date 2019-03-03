a2994eb74eac569793fd562d401f1ad0342e5f89
mading <mading@bjtu.edu.cn>
2019-03-03 21:09:56 +0800

    Update mergeTwoSortedArray.cpp
    add C++ solution

--- algorithms/cpp/mergeTwoSortedArray/mergeTwoSortedArray.cpp
+++ algorithms/cpp/mergeTwoSortedArray/mergeTwoSortedArray.cpp
@@ -13,6 +13,25 @@
 *               
 **********************************************************************************/
 
+
+//4 ms  9.2 MB
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        while(n>0){
+            if(m==0 || nums1[m-1] <= nums2[n-1]){
+                nums1[m+n-1] = nums2[n-1];
+                n--;
+            }
+            else{
+                nums1[m+n-1] = nums1[m-1];
+                m--;
+            }
+        }
+        return ;
+    }
+
+
+/*******Old solution in C*********************/
+
 #include <stdio.h>
 
 void merge(int A[], int m, int B[], int n) {
