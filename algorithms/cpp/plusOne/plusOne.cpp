22745f9e77bbb59602bb6479b7d3185804bdcd26
mading <mading@bjtu.edu.cn>
2019-03-03 21:07:36 +0800

    Update plusOne.cpp
    update Runtime and Memory Usage

--- algorithms/cpp/plusOne/plusOne.cpp
+++ algorithms/cpp/plusOne/plusOne.cpp
@@ -1,12 +1,27 @@
-// Source : https://oj.leetcode.com/problems/plus-one/
+// Source : https://leetcode.com/problems/plus-one/
 // Author : Hao Chen
 // Date   : 2014-06-21
+//
+// Latest Edition
+// Editor : Mading
+// Date   : 2019-03-03
 
 /********************************************************************************** 
-* 
-* Given a non-negative number represented as an array of digits, plus one to the number.
-* 
-* The digits are stored such that the most significant digit is at the head of the list.
+* 66. Plus One
+* Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+* The digits are stored such that the most significant digit is at the head of the list, and each
+* element in the array contain a single digit.
+* You may assume the integer does not contain any leading zero, except the number 0 itself.
+
+* Example 1:
+* Input: [1,2,3]
+* Output: [1,2,4]
+* Explanation: The array represents the integer 123.
+
+* Example 2:
+* Input: [4,3,2,1]
+* Output: [4,3,2,2]
+* Explanation: The array represents the integer 4321.
 *               
 **********************************************************************************/
 
@@ -14,6 +29,25 @@
 #include <vector>
 using namespace std;
 
+//4 ms  8.5 MB
+vector<int> plusOne(vector<int>& digits) {
+       for(int i = digits.size()-1;i>=0;i--){
+           if(digits[i] == 9){
+               digits[i] = 0;
+               if(i == 0){
+                   digits.insert(digits.begin(),1);
+                   break;
+               }
+           }
+           else {
+               digits[i] += 1;
+               break;
+           }
+       }
+        return digits;
+    }
+
+//8 ms  8.7 MB
 vector<int> plusOne(vector<int> &digits) {
     int carry=1;
     vector <int> v;
