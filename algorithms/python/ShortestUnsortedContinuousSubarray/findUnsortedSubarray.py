859e817fa970d2b776aed4d045737bc13e63e502
madingx <mading@bjtu.edu.cn>
2019-03-15 10:27:08 +0800

    581. Shortest Unsorted Continuous Subarray

--- algorithms/python/ShortestUnsortedContinuousSubarray/findUnsortedSubarray.py
+++ algorithms/python/ShortestUnsortedContinuousSubarray/findUnsortedSubarray.py
@@ -1,3 +1,4 @@
+#80 ms 14.2 MB
 def findUnsortedSubarray(self, nums):
     same = [a == b for a, b in zip(nums, sorted(nums))]
     return 0 if all(same) else len(nums) - same.index(False) - same[::-1].index(False)
