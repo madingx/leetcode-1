cb14f8e743e93077160f4993abbd54b473b5dd66
madingx <mading@bjtu.edu.cn>
2019-03-18 09:06:22 +0800

    Update validPalindrome.cpp

--- algorithms/cpp/validPalindrome/validPalindrome.cpp
+++ algorithms/cpp/validPalindrome/validPalindrome.cpp
@@ -1,25 +1,26 @@
-// Source : https://oj.leetcode.com/problems/valid-palindrome/
+// Source : https://leetcode.com/problems/valid-palindrome/submissions/
 // Author : Hao Chen
 // Date   : 2014-06-26
 
 /********************************************************************************** 
-* 
+* 125. Valid Palindrome [easy]
 * Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 * 
-* For example,
-* "A man, a plan, a canal: Panama" is a palindrome.
-* "race a car" is not a palindrome.
-* 
-* Note:
-* Have you consider that the string might be empty? This is a good question to ask during an interview.
-* 
-* For the purpose of this problem, we define empty string as valid palindrome.
-* 
+* Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+* Example 1:
+* Input: "A man, a plan, a canal: Panama"
+* Output: true
+
+* Example 2:
+* Input: "race a car"
+* Output: false
 *               
 **********************************************************************************/
 
 class Solution {
 public:
+    //12 ms 9.6 MB, faster than 77.92% of C++
     bool isPalindrome(string s) {
         s = removeNoise(s);
         for(int i=0; i<s.size()/2; i++){
