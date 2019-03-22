89582ba661137b84583d2d734078fb3e2f3d4672
madingx <mading@bjtu.edu.cn>
2019-03-22 10:17:50 +0800

    Update numberOf1Bits.cpp

--- algorithms/cpp/numberOf1Bits/numberOf1Bits.cpp
+++ algorithms/cpp/numberOf1Bits/numberOf1Bits.cpp
@@ -3,24 +3,60 @@
 // Date   : 2015-03-30
 
 /********************************************************************************** 
-* 
-* Write a function that takes an unsigned integer and returns the number of ’1' bits it has 
-* (also known as the Hamming weight).
-* 
-* For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, 
-* so the function should return 3.
-* 
-* Credits:Special thanks to @ts for adding this problem and creating all test cases.
+* 191. Number of 1 Bits [Easy]
+* Write a function that takes an unsigned integer and return the number of '1' bits
+* it has (also known as the Hamming weight).
+
+* Example 1:
+* Input: 00000000000000000000000000001011
+* Output: 3
+* Explanation: The input binary string 00000000000000000000000000001011 
+* has a total of three '1' bits.
+
+* Example 2:
+* Input: 00000000000000000000000010000000
+* Output: 1
+* Explanation: The input binary string 00000000000000000000000010000000 
+* has a total of one '1' bit.
+
+* Example 3:
+* Input: 11111111111111111111111111111101
+* Output: 31
+* Explanation: The input binary string 11111111111111111111111111111101 
+* has a total of thirty one '1' bits.
+ 
+* Note:
+* Note that in some languages such as Java, there is no unsigned integer type.
+* In this case, the input will be given as signed integer type and should not 
+* affect your implementation, as the internal binary representation of the 
+* integer is the same whether it is signed or unsigned.
+
+* In Java, the compiler represents the signed integers using 2's complement 
+* notation. Therefore, in Example 3 above the input represents the signed integer -3.
+ 
+* Follow up:
+* If this function is called many times, how would you optimize it?
 *               
 **********************************************************************************/
 
 class Solution {
 public:
+    //4 ms  8.2 MB, faster than 100.00% of C++
     int hammingWeight(uint32_t n) {
         int cnt = 0;
         for(;n>0; n/=2){
             if (n & 0x1) cnt++;
         }
         return cnt;
+    }
+
+    //4 ms  8.1 MB, faster than 100.00% of C++
+    int hammingWeight(uint32_t n) {
+        int NumberOfOne = 0;
+        while(n) {
+            NumberOfOne ++;
+            n = n&(n-1);
+        }
+        return NumberOfOne;
     }
 };
