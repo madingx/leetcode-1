4b6986c5c064ff668436f5b70bbbbd3a6d4d1352
madingx <mading@bjtu.edu.cn>
2019-04-08 14:14:47 +0800

    91. Decode Ways [Medium]

--- algorithms/cpp/decodeWays/decodeWays.cpp
+++ algorithms/cpp/decodeWays/decodeWays.cpp
@@ -1,9 +1,9 @@
-// Source : https://oj.leetcode.com/problems/decode-ways/
+// Source : https://leetcode.com/problems/decode-ways/
 // Author : Hao Chen
 // Date   : 2014-07-16
 
 /********************************************************************************** 
-* 
+* 91. Decode Ways [Medium]
 * A message containing letters from A-Z is being encoded to numbers using the following mapping:
 * 
 * 'A' -> 1
@@ -13,12 +13,15 @@
 * 
 * Given an encoded message containing digits, determine the total number of ways to decode it.
 * 
-* For example,
-* Given encoded message "12",
-* it could be decoded as "AB" (1 2) or "L" (12).
-* 
-* The number of ways decoding "12" is 2.
-* 
+* Example 1:
+* Input: "12"
+* Output: 2
+* Explanation: It could be decoded as "AB" (1 2) or "L" (12).
+
+* Example 2:
+* Input: "226"
+* Output: 3
+* Explanation: It could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
 *               
 **********************************************************************************/
 
@@ -27,6 +30,23 @@
 #include <string.h>
 using namespace std;
 
+// 4 ms, faster than 100.00% of C++, 8.2 MB, less than 94.57% of C++ 
+class Solution {
+public:
+    int numDecodings(string s) {
+        int p = 1, pp, n = s.size();
+        for(int i=n-1;i>=0;i--) {
+            int cur = s[i]=='0' ? 0 : p;
+            if(i<n-1 && (s[i]=='1'||s[i]=='2'&&s[i+1]<'7')) cur+=pp;
+            pp = p;
+            p = cur;
+        }
+        return s.empty()? 0 : p;   
+    }
+};
+
+
+// 8 ms, faster than 35.66% of C++  8.3 MB, less than 88.29% of C++ 
 
 //Count[i] = Count[i-1]              if S[i-1] is a valid char (not '0')
 //         = Count[i-1]+ Count[i-2]  if S[i-1] and S[i-2] together is still a valid char (10 to 26).
