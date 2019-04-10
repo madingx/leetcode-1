c5d37bf9223c42d7635b45bd16db0822b436479d
madingx <mading@bjtu.edu.cn>
2019-04-10 09:58:38 +0800

    343. Integer Break [Medium]

--- algorithms/cpp/integerBreak/IntegerBreak.cpp
+++ algorithms/cpp/integerBreak/IntegerBreak.cpp
@@ -3,21 +3,21 @@
 // Date   : 2016-05-29
 
 /*************************************************************************************** 
- *
+ * 343. Integer Break [Medium]
  * Given a positive integer n, break it into the sum of at least two positive integers 
  * and maximize the product of those integers. Return the maximum product you can get.
- * 
- * For example, given n = 2, return 1 (2 = 1 + 1); given n = 10, return 36 (10 = 3 + 3 
- * + 4).
- * 
- * Note: you may assume that n is not less than 2.
- * 
- *   There is a simple O(n) solution to this problem.
- *   You may check the breaking results of n ranging from 7 to 10 to discover the 
- * regularities.
- * 
- * Credits:Special thanks to @jianchao.li.fighter for adding this problem and creating 
- * all test cases.
+
+ * Example 1:
+ * Input: 2
+ * Output: 1
+ * Explanation: 2 = 1 + 1, 1 × 1 = 1.
+
+ * Example 2:
+ * Input: 10
+ * Output: 36
+ * Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ 
+ * Note: You may assume that n is not less than 2 and not larger than 58.
  ***************************************************************************************/
 
 class Solution {
@@ -31,6 +31,7 @@ public:
     //
     // we can see we can break the number by 3 if it is greater than 4;
     //
+    // Runtime: 4 ms, faster than 100.00% of C++, 8.3 MB, less than 69.32% of C++
     int integerBreak(int n) {
         if ( n == 2 ) return 1;
         if ( n == 3 ) return 2;
@@ -42,5 +43,55 @@ public:
         result *= n;
         return result;
     }
+
+    // O(n) solution
+    // 4 ms, faster than 100.00% of C++ , 8.3 MB, less than 70.45% of C++ 
+    int integerBreak(int n) {
+        int dp[n>4? n + 1:5];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 4;
+        for (int i = 5; i <= n; ++i) {
+            dp[i] = 3 * max(i - 3, dp[i - 3]);
+        }
+        return dp[n];
+        
+    }
+
+    // O(1) solution
+    // 4 ms, faster than 100.00% of C++, 8.4 MB, less than 42.04% of C++ 
+    int integerBreak(int n) {
+        if (n==2)
+            return 1;
+        if (n==3)
+            return 2;
+        if (n%3 == 0)
+        {
+            return pow(3,n/3);
+        }
+        if (n%3 == 2)
+        {
+            return pow(3,n/3)*2;
+        }
+        return pow(3,(n/3)-1)*4;
+        
+    }
+    /*
+        2 - 1,1
+        3 - 1,2
+        4 - 2,2
+        5 - 3,2
+        6 - 3,3
+        7 - 3,4
+        8 - 3,3,2
+        9 - 3,3,3
+        10 - 3,3,4
+        11 - 3,3,3,2
+        12 - 3,3,3,3
+        13 - 3,3,3,4
+*/
+
 };
 
