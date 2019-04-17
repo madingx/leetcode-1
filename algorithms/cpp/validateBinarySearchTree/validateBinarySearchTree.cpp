2ababe3d4534ba60ad43a73d15151c3e89e700a8
madingx <mading@bjtu.edu.cn>
2019-04-17 14:30:08 +0800

    98. Validate Binary Search Tree [Medium]

--- algorithms/cpp/validateBinarySearchTree/validateBinarySearchTree.cpp
+++ algorithms/cpp/validateBinarySearchTree/validateBinarySearchTree.cpp
@@ -1,16 +1,16 @@
-// Source : https://oj.leetcode.com/problems/validate-binary-search-tree/
+// Source : https://leetcode.com/problems/validate-binary-search-tree/
 // Author : Hao Chen
 // Date   : 2014-07-05
 
 /********************************************************************************** 
-* 
+* 98. Validate Binary Search Tree [Medium]
 * Given a binary tree, determine if it is a valid binary search tree (BST).
 * 
 * Assume a BST is defined as follows:
 * 
-* The left subtree of a node contains only nodes with keys less than the node's key.
-* The right subtree of a node contains only nodes with keys greater than the node's key.
-* Both the left and right subtrees must also be binary search trees.
+* 1.The left subtree of a node contains only nodes with keys less than the node's key.
+* 2.The right subtree of a node contains only nodes with keys greater than the node's key.
+* 3.Both the left and right subtrees must also be binary search trees.
 * 
 * confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
 * 
@@ -19,17 +19,22 @@
 * The serialization of a binary tree follows a level order traversal, where '#' signifies 
 * a path terminator where no node exists below.
 * 
-* Here's an example:
-* 
-*    1
-*   / \
-*  2   3
-*     /
-*    4
-*     \
-*      5
-* 
-* The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}". 
+* Example 1:
+* Input:
+*     2
+*    / \
+*   1   3
+* Output: true
+
+* Example 2:
+*     5
+*    / \
+*   1   4
+*      / \
+*     3   6
+* Output: false
+* Explanation: The input is: [5,1,4,null,null,3,6]. The root node's value
+*             is 5 but its right child's value is 4.
 * 
 *               
 **********************************************************************************/
@@ -46,6 +51,21 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// 20 ms, faster than 99.10% of C++, 20.4 MB, less than 95.79% of C++
+bool isValidBST(TreeNode* root) {
+    TreeNode* prev = NULL;
+    return validate(root, prev);
+}
+bool validate(TreeNode* node, TreeNode* &prev) {
+    if (node == NULL) return true;
+    if (!validate(node->left, prev)) return false;
+    if (prev != NULL && prev->val >= node->val) return false;
+    prev = node;
+    return validate(node->right, prev);
+}
+
+
+// 20 ms, faster than 99.10% of C++, 20.8 MB, less than 21.53% of C++ 
 bool isValidBST(TreeNode *root) {
 
     //travel the tree by inner-order
