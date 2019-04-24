8dcb598fe8d79cad2ffdab2eba4400387d05e7d3
madingx <mading@bjtu.edu.cn>
2019-04-24 14:51:12 +0800

    99. Recover Binary Search Tree [Hard]

--- algorithms/cpp/recoverBinarySearchTree/recoverBinarySearchTree.cpp
+++ algorithms/cpp/recoverBinarySearchTree/recoverBinarySearchTree.cpp
@@ -1,35 +1,46 @@
-// Source : https://oj.leetcode.com/problems/recover-binary-search-tree/
+// Source : https://leetcode.com/problems/recover-binary-search-tree/
 // Author : Hao Chen
 // Date   : 2014-10-11
 
 /********************************************************************************** 
-* 
+* 99. Recover Binary Search Tree [Hard]
 * Two elements of a binary search tree (BST) are swapped by mistake.
-* 
 * Recover the tree without changing its structure.
 * 
-* Note:
-* A solution using O(n) space is pretty straight forward. Could you devise a constant space solution?
-* 
-* confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
-* 
-* OJ's Binary Tree Serialization:
-* 
-* The serialization of a binary tree follows a level order traversal, where '#' signifies 
-* a path terminator where no node exists below.
-* 
-* Here's an example:
-* 
+* Example 1:
+* Input: [1,3,null,null,2]
+
 *    1
-*   / \
-*  2   3
-*     /
-*    4
-*     \
-*      5
-* 
-* The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}". 
-* 
+*   /
+*  3
+*   \
+*    2
+* Output: [3,1,null,null,2]
+
+*    3
+*   /
+*  1
+*   \
+*    2
+
+* Example 2:
+* Input: [3,1,4,null,null,2]
+
+*   3
+*  / \
+* 1   4
+*    /
+*   2
+* Output: [2,1,4,null,null,3]
+*   2
+*  / \
+* 1   4
+*    /
+*   3
+
+* Follow up:
+* A solution using O(n) space is pretty straight forward.
+* Could you devise a constant space solution? 
 *               
 **********************************************************************************/
 
@@ -59,10 +70,10 @@
 //  1   3     5   7     9  11    13  15
 //  
 //  
+// 44 ms, faster than 69.65% of C++, 17.5 MB, less than 100.00% of C++ 
 class Solution {
 public:
     void recoverTreeHelper(TreeNode *root) {
-        
         if (root == NULL) return;
         
         recoverTreeHelper(root->left);
@@ -87,4 +98,40 @@ public:
     }
 private:
     TreeNode *n1, *n2, *prev;
+};
+
+
+// 32 ms, faster than 97.10% of C++ 17.8 MB, less than 98.44% of C++ 
+class Solution {
+    TreeNode *first;
+    TreeNode *second;
+    TreeNode *pre;
+public:
+    void recoverTree(TreeNode* root){
+        first = NULL;
+        second = NULL;
+        pre = new TreeNode(INT_MIN);
+
+        traverse(root);
+        
+        int tmp = first->val;
+        first->val = second->val;
+        second->val = tmp;
+        delete(pre);
+        return;
+    }
+        
+    void traverse(TreeNode* root) {
+        if (!root) return;
+        traverse(root->left);
+        if(!first && pre->val>=root->val)  
+            first = pre;
+        if(first && pre->val>=root->val)  
+            second = root;
+        pre = root;
+        
+        traverse(root->right);
+        return;
+        
+    }
 };
