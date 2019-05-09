01b8fe10479ec9623bf03b13df01f607f84e02ad
madingx <mading@bjtu.edu.cn>
2019-05-09 21:17:19 +0800

    225. Implement Stack using Queues [Easy]

--- algorithms/cpp/implementStackUsingQueues/ImplementStackUsingQueues.cpp
+++ algorithms/cpp/implementStackUsingQueues/ImplementStackUsingQueues.cpp
@@ -3,7 +3,7 @@
 // Date   : 2015-06-13
 
 /********************************************************************************** 
- * 
+ * 225. Implement Stack using Queues [Easy]
  * Implement the following operations of a stack using queues.
  * 
  * push(x) -- Push element x onto stack.
@@ -63,4 +63,41 @@ class Stack {
 
     private:
         queue<int> nums;
+};
+
+
+// 0 ms, faster than 100.00% of C++, 8.8 MB, less than 88.68% of C++
+class MyStack {
+public:
+    queue<int> que;
+    /** Initialize your data structure here. */
+    MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        que.push(x);
+        for(int i=0;i<que.size()-1;++i){
+            que.push(que.front());
+            que.pop();
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int tmp = que.front();
+        que.pop();
+        return tmp;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return que.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return que.empty();
+    }
 };
