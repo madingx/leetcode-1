91375c3e0a53699d449f459a06bcc03a6e043b4a
madingx <mading@bjtu.edu.cn>
2019-05-15 14:34:37 +0800

    220. Contains Duplicate III [Medium]

--- algorithms/cpp/containsDuplicate/ContainsDuplicate.II.cpp
+++ algorithms/cpp/containsDuplicate/ContainsDuplicate.II.cpp
@@ -3,14 +3,25 @@
 // Date   : 2015-06-12
 
 /********************************************************************************** 
- * 
+ * 219. Contains Duplicate II [Easy]
  * Given an array of integers and an integer k, find out whether there there are 
  * two distinct indices i and j in the array such that nums[i] = nums[j] and 
  * the difference between i and j is at most k.
- * 
+
+ * Example 1:
+ * Input: nums = [1,2,3,1], k = 3
+ * Output: true
+
+ * Example 2:
+ * Input: nums = [1,0,1,1], k = 1
+ * Output: true
+ 
+ * Example 3:
+ * Input: nums = [1,2,3,1,2,3], k = 2
+ * Output: false
  *               
  **********************************************************************************/
-
+// 28 ms, faster than 97.26%, 15.6 MB, less than 8.39% of C++
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
