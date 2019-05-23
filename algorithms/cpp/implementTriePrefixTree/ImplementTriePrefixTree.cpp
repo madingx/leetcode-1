519dbf6b0560dcb070831c89b60f1fedee857a34
madingx <mading@bjtu.edu.cn>
2019-05-23 14:55:32 +0800

    208. Implement Trie (Prefix Tree) [Medium]

--- algorithms/cpp/implementTriePrefixTree/ImplementTriePrefixTree.cpp
+++ algorithms/cpp/implementTriePrefixTree/ImplementTriePrefixTree.cpp
@@ -3,8 +3,23 @@
 // Date   : 2015-06-09
 
 /********************************************************************************** 
- * 
+ * 208. Implement Trie (Prefix Tree) [Medium]
  * Implement a trie with insert, search, and startsWith methods.
+
+ * Example:
+
+ * Trie trie = new Trie();
+
+ * trie.insert("apple");
+ * trie.search("apple");   // returns true
+ * trie.search("app");     // returns false
+ * trie.startsWith("app"); // returns true
+ * trie.insert("app");   
+ * trie.search("app");     // returns true
+ * Note:
+
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * All inputs are guaranteed to be non-empty strings.
  * 
  * Note:
  * You may assume that all inputs are consist of lowercase letters a-z.
@@ -12,7 +27,7 @@
  *               
  **********************************************************************************/
 
-
+// 104 ms, faster than 32.39% of C++, 36 MB, less than 83.86% of C++
 class TrieNode {
 public:
     // Initialize your data structure here.
@@ -72,3 +87,71 @@ private:
 // Trie trie;
 // trie.insert("somestring");
 // trie.search("key");
+
+
+
+
+
+// 80 ms, faster than 86.65% of C++, 48.3 MB, less than 18.07% of C++
+class TrieNode {
+public:
+    bool has;
+    vector<TrieNode*> children;
+    TrieNode(){
+        has = false;
+        children = vector<TrieNode*>(26,NULL);
+    }
+    ~TrieNode(){
+        for(auto i:children){
+            delete i;
+        }
+    }
+};
+
+class Trie {
+    TrieNode* root;
+public:
+    /** Initialize your data structure here. */
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    void insert(string word) {
+        TrieNode* cur = root;
+        for(auto c:word){
+            if( !cur->children[c-'a'] ) cur->children[c-'a'] = new TrieNode();             
+            cur = cur->children[c-'a'];
+        }
+        cur->has = true;
+        return;
+    }
+    
+    /** Returns if the word is in the trie. */
+    bool search(string word) {
+        TrieNode* cur = root;
+        for(auto c:word){
+            if( cur->children[c-'a'] )   cur = cur->children[c-'a'];
+            else  return false;
+        }
+        return cur->has;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    bool startsWith(string prefix) {
+        TrieNode* cur = root;
+        for(auto c:prefix){
+            if( cur->children[c-'a'] )   cur = cur->children[c-'a'];
+            else  return false;
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
