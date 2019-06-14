3c8ea5420f373be9a1dda76fbb08882dc9dcbd69
madingx <mading@bjtu.edu.cn>
2019-06-14 10:10:03 +0800

    332. Reconstruct Itinerary [Medium]

--- algorithms/cpp/reconstructItinerary/ReconstructItinerary.cpp
+++ algorithms/cpp/reconstructItinerary/ReconstructItinerary.cpp
@@ -3,27 +3,27 @@
 // Date   : 2017-01-06
 
 /*************************************************************************************** 
- *
+ * 332. Reconstruct Itinerary [Medium]
  * Given a list of airline tickets represented by pairs of departure and arrival 
  * airports [from, to], reconstruct the itinerary in order. All of the tickets belong 
  * to a man who departs from JFK. Thus, the itinerary must begin with JFK.
  * 
  * Note:
  * 
- * If there are multiple valid itineraries, you should return the itinerary that has 
+ * 1. If there are multiple valid itineraries, you should return the itinerary that has 
  * the smallest lexical order when read as a single string. For example, the itinerary 
  * ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
- * All airports are represented by three capital letters (IATA code).
- * You may assume all tickets form at least one valid itinerary.
+ * 2. All airports are represented by three capital letters (IATA code).
+ * 3. You may assume all tickets form at least one valid itinerary.
  * 
- *     Example 1:
- *     tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
- *     Return ["JFK", "MUC", "LHR", "SFO", "SJC"].
+ * Example 1:
+ * tickets = [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]
+ * Return ["JFK", "MUC", "LHR", "SFO", "SJC"].
  * 
- *     Example 2:
- *     tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
- *     Return ["JFK","ATL","JFK","SFO","ATL","SFO"].
- *     Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"]. But it 
+ * Example 2:
+ * tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * Return ["JFK","ATL","JFK","SFO","ATL","SFO"].
+ * Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"]. But it 
  * is larger in lexical order.
  * 
  * Credits:Special thanks to @dietpepsi for adding this problem and creating all test 
@@ -52,7 +52,7 @@
  Then, it's easy to have a DFS algorithm.
 */
 
-
+// 40 ms, faster than 30.08% of C++, 17.8 MB, less than 25.96% of C++
 class Solution {
 public:
     //DFS
@@ -74,5 +74,30 @@ public:
         string start = "JFK";
         travel(start, map, result);
         return result;
+    }
+};
+
+
+
+// 32 ms, faster than 49.05% of C++, 19 MB, less than 11.25% of C++ 
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>> tickets) {
+        for (auto ticket : tickets)
+            targets[ticket[0]].insert(ticket[1]);
+        visit("JFK");
+        return vector<string>(route.rbegin(), route.rend());
+    }
+
+    map<string, multiset<string>> targets;
+    vector<string> route;
+
+    void visit(string airport) {
+        while (targets[airport].size()) {
+            string next = *targets[airport].begin();
+            targets[airport].erase(targets[airport].begin());
+            visit(next);
+        }
+        route.push_back(airport);
     }
 };
