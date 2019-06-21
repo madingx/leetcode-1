4f00a83dde4b205212c43b0045311a41d4d5c826
madingx <mading@bjtu.edu.cn>
2019-06-21 15:18:58 +0800

    28. Implement strStr() [Easy]

--- algorithms/cpp/strStr/strStr.cpp
+++ algorithms/cpp/strStr/strStr.cpp
@@ -3,13 +3,96 @@
 // Date   : 2014-07-19
 
 /********************************************************************************** 
-* 
+* 28. Implement strStr() [Easy]
 * Implement strStr().
-* 
-* Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part of haystack.
-* 
+
+* Return the index of the first occurrence of needle in haystack, 
+* or -1 if needle is not part of haystack.
+
+* Example 1:
+
+* Input: haystack = "hello", needle = "ll"
+* Output: 2
+* Example 2:
+
+* Input: haystack = "aaaaa", needle = "bba"
+* Output: -1
+* Clarification:
+
+* What should we return when needle is an empty string? 
+* This is a great question to ask during an interview.
+
+* For the purpose of this problem, we will return 0 when needle is an empty string.
+* This is consistent to C's strstr() and Java's indexOf().
 *               
 **********************************************************************************/
+
+
+// 4 ms, faster than 96.09% of C++, 9.1 MB, less than 51.40% of C++ 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size(), p = 0;
+        while (p + n - 1 < m) {
+            if (haystack.substr(p, n) == needle) {
+                return p;
+            }
+            while (p++ + n - 1 < m && haystack[p] != needle[0]);
+        }
+        return -1;
+    }
+};
+
+
+
+// KMP
+// 4 ms, faster than 96.09% of C++, 9.2 MB, less than 40.71% of C++
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.size(), n = needle.size();
+        if (!n) {
+            return 0;
+        }
+        vector<int> lps = kmpProcess(needle);
+        for (int i = 0, j = 0; i < m;) {
+            if (haystack[i] == needle[j]) { 
+                i++, j++;
+            }
+            if (j == n) {
+                return i - j;
+            }
+            if (i < m && haystack[i] != needle[j]) {
+                j ? j = lps[j - 1] : i++;
+            }
+        }
+        return -1;
+    }
+private:
+    vector<int> kmpProcess(string needle) {
+        int n = needle.size();
+        vector<int> lps(n, 0);
+        for (int i = 1, len = 0; i < n;) {
+            if (needle[i] == needle[len]) {
+                lps[i++] = ++len;
+            } else if (len) {
+                len = lps[len - 1];
+            } else {
+                lps[i++] = 0;
+            }
+        }
+        return lps;
+    }
+
+
+
+
+
+
+
+
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
