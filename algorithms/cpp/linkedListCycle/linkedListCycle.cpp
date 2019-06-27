8a51b9a46164c4e9ceb787e46e222b6328b5d04b
madingx <mading@bjtu.edu.cn>
2019-06-27 09:33:48 +0800

    141. Linked List Cycle [Easy]

--- algorithms/cpp/linkedListCycle/linkedListCycle.cpp
+++ algorithms/cpp/linkedListCycle/linkedListCycle.cpp
@@ -1,9 +1,9 @@
-// Source : https://oj.leetcode.com/problems/linked-list-cycle/
+// Source : https://leetcode.com/problems/linked-list-cycle/
 // Author : Hao Chen
 // Date   : 2014-07-03
 
 /********************************************************************************** 
-* 
+* 141. Linked List Cycle [Easy]
 * Given a linked list, determine if it has a cycle in it.
 * 
 * Follow up:
@@ -11,6 +11,15 @@
 * 
 *               
 **********************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
 
 /*
  * if there is a cycle in the list, then we can use two pointers travers the list.
@@ -20,13 +29,44 @@
  * so, those two pointers meet together, that means there must be a cycle inside the list.
  */
 
-bool hasCycle(ListNode *head) {
-    if (head==NULL || head->next==NULL) return false;
-    ListNode* fast=head;
-    ListNode* slow=head;
-    do{
-        slow = slow->next;
-        fast = fast->next->next;
-    }while(fast != NULL && fast->next != NULL && fast != slow);
-    return fast == slow? true : false;
-} 
+// similar
+// 12 ms, faster than 86.20% of C++, 9.8 MB, less than 27.09% of C++
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head==NULL) return false;
+        if(head->next==NULL) return false;
+        if(head->next->next==NULL) return false;
+        
+        ListNode *slow=head;
+        ListNode *fast=head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};  
+
+
+// another method
+// 20 ms, faster than 25.62% of C++, 12.5 MB, less than 5.05% of C++
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_map<ListNode*,bool> nodemap;
+        while(head){
+            if(nodemap.find(head) != nodemap.end())return true;
+            nodemap[head] = true;
+            head = head->next;
+        }
+        return false;
+    }
+};
+
+
