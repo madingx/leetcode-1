812805f3e0be02c495b3a94c27f4d4ddd0a40922
madingx <mading@bjtu.edu.cn>
2019-07-01 21:31:10 +0800

    19. Remove Nth Node From End of List [Medium]

--- algorithms/cpp/removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.cpp
+++ algorithms/cpp/removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.cpp
@@ -1,9 +1,9 @@
-// Source : https://oj.leetcode.com/problems/remove-nth-node-from-end-of-list/
+// Source : https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 // Author : Hao Chen
 // Date   : 2014-06-21
 
 /********************************************************************************** 
-* 
+* 19. Remove Nth Node From End of List [Medium]
 * Given a linked list, remove the nth node from the end of list and return its head.
 * 
 * For example,
@@ -27,6 +27,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// 8 ms, faster than 26.11% of C++, 8.6 MB, less than 41.29% of C++
 class Solution {
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
@@ -50,5 +51,58 @@ public:
         
         p1->next = p1->next->next;
         return head->next;
+    }
+};
+
+
+
+// 12 ms, faster than 9.01% of C++, 8.4 MB, less than 82.71% of C++
+class Solution {
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        ListNode *p1 = head,*p2 = head;
+        int i = 0;
+        for(;i<n+1 && p2;i++){
+            p2 = p2->next;
+        }
+        if(i < n+1)return p1->next;
+        for(;p2;i++){
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        p1->next = p1->next->next;
+        return head;
+        
+    }
+};
+
+
+
+// 8 ms, faster than 26.11% of C++, 8.6 MB, less than 24.72% of C++
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        int len=1;
+        ListNode *h1=head;
+        while(h1->next!=NULL){
+            len++;
+            h1=h1->next;
+        }
+        h1=head;
+        
+        for(int i=0;i<len-n-1;i++){
+            h1=h1->next;
+        }
+        
+        if(len-n-1<0) return head->next;
+        
+        if(len>=2){
+        ListNode *q=h1->next->next;
+        h1->next=q;
+        }
+        
+        else head=NULL;
+
+        return head;
     }
 };
