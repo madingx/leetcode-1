f4ad94ea731a6fbbda845eece172147917ab05ae
madingx <mading@bjtu.edu.cn>
2019-09-27 22:14:59 +0800

    50. Pow(x, n) [Medium]

--- algorithms/cpp/pow/pow.cpp
+++ algorithms/cpp/pow/pow.cpp
@@ -1,13 +1,59 @@
-// Source : https://oj.leetcode.com/problems/powx-n/
+// Source : https://leetcode.com/problems/powx-n/
 // Author : Hao Chen
 // Date   : 2014-06-25
 
 /********************************************************************************** 
-* 
-* Implement pow(x, n).
-* 
+* 50. Pow(x, n) [Medium]
+* Implement pow(x, n), which calculates x raised to the power n (xn).
+
+* Example 1:
+
+* Input: 2.00000, 10
+* Output: 1024.00000
+* Example 2:
+
+* Input: 2.10000, 3
+* Output: 9.26100
+* Example 3:
+
+* Input: 2.00000, -2
+* Output: 0.25000
+* Explanation: 2-2 = 1/22 = 1/4 = 0.25
+* Note:
+
+* -100.0 < x < 100.0
+* n is a 32-bit signed integer, within the range [−231, 231 − 1]
 *               
 **********************************************************************************/
+
+
+
+
+// 0 ms, faster than 100.00% of C++, 8.2 MB, less than 100.00% of C++ 
+class Solution {
+public:
+    double myPow(double x, long long int  n) {
+        if (n == 0){
+            return 1.0;
+        }
+        if ( n == -1){
+            return 1.0/x;
+        }
+        double hp= myPow(x,n/2);
+        int np = abs(n);
+        if (np%2 == 1){
+            double m = n > 0 ? x:(1.0/x);
+            return m*hp*hp;
+        }
+        else{
+            return hp*hp;
+        }
+    }
+};
+
+
+
+
 
 #include <stdio.h>
 #include <stdlib.h>
