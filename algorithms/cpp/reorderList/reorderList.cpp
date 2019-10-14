5b27d097e7cc645e63de197ca90cae4c3c9f2a58
madingx <mading@bjtu.edu.cn>
2019-10-14 10:26:27 +0800

    143. Reorder List [Medium]

--- algorithms/cpp/reorderList/reorderList.cpp
+++ algorithms/cpp/reorderList/reorderList.cpp
@@ -1,20 +1,86 @@
-// Source : https://oj.leetcode.com/problems/reorder-list/
-// Author : Hao Chen
+// Source : https://leetcode.com/problems/reorder-list/
+// Author : Hao Chen, Mading
 // Date   : 2014-06-17
 
 /********************************************************************************** 
-* 
+* 143. Reorder List [Medium]
 * Given a singly linked list L: L0→L1→…→Ln-1→Ln,
 * reorder it to: L0→Ln→L1→Ln-1→L2→Ln-2→…
-* 
-* You must do this in-place without altering the nodes' values.
-* 
-* For example,
-* Given {1,2,3,4}, reorder it to {1,4,2,3}.
-* 
+
+* You may not modify the values in the list's nodes, only nodes itself may be changed.
+
+* Example 1:
+* Given 1->2->3->4, reorder it to 1->4->2->3.
+
+* Example 2:
+* Given 1->2->3->4->5, reorder it to 1->5->2->4->3.
 *               
 **********************************************************************************/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 
+// Mading   //recursion
+// 48 ms, faster than 77.47% of C++, 13.4 MB, less than 11.76% of C++
+class Solution {
+    bool reorderListRec(ListNode* &pre,ListNode *head){
+        if(!pre || !head)return false;        
+        bool newend = reorderListRec(pre,head->next); //是否已产生新的结尾
+        if(newend)return newend;
+        if(pre == head || pre->next == head){
+            head->next = NULL;
+            return true;
+        }     
+        ListNode* t = pre->next; 
+        pre->next = head;
+        head->next = t;
+        pre = t;  
+        return false;
+    }
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next)return;
+        reorderListRec(head,head->next);
+    }
+};
+
+
+//Mading   //dp
+// 48 ms, faster than 77.47% of C++, 13.1 MB, less than 17.65% of C++
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next)return;
+        vector<ListNode*> vec;
+        while(head){
+            vec.push_back(head);
+            head = head->next;
+        }
+        int left = 0,right = vec.size()-1;
+        while(left <= right){
+            vec[left]->next = vec[right];
+            left++;
+            vec[right]->next = vec[left];
+            right--;
+        }
+        vec[vec.size()/2]->next = NULL;
+        return;
+    }
+};
+
+
+
+
+
+
+
+// Hao Chen
+// 44 ms, faster than 95.23% of C++, 12 MB, less than 100.00% of C++
 #include <stdio.h>
 #include <stdlib.h>
 /**
