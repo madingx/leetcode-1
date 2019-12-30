da5b5de3c7881e0d1d7239d4e88067234e365c0a
madingx <mading@bjtu.edu.cn>
2019-12-30 14:52:38 +0800

    10. Regular Expression Matching

--- algorithms/cpp/regularExpressionMatching/regularExpressionMatching.cpp
+++ algorithms/cpp/regularExpressionMatching/regularExpressionMatching.cpp
@@ -1,30 +1,109 @@
-// Source : https://oj.leetcode.com/problems/regular-expression-matching/
-// Author : Hao Chen
-// Date   : 2014-08-24
+// Source : https://leetcode.com/problems/regular-expression-matching/
+// Author : Hao Chen, Mading
+// Date   : 2014-08-24, 2019-12-30
 
 /********************************************************************************** 
-* 
-* Implement regular expression matching with support for '.' and '*'.
-* 
+* 10. Regular Expression Matching [Hard]
+* Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+
 * '.' Matches any single character.
 * '*' Matches zero or more of the preceding element.
-* 
 * The matching should cover the entire input string (not partial).
-* 
-* The function prototype should be:
-* bool isMatch(const char *s, const char *p)
-* 
-* Some examples:
-* isMatch("aa","a") → false
-* isMatch("aa","aa") → true
-* isMatch("aaa","aa") → false
-* isMatch("aa", "a*") → true
-* isMatch("aa", ".*") → true
-* isMatch("ab", ".*") → true
-* isMatch("aab", "c*a*b") → true
-* 
-*               
+
+* Note:
+
+* s could be empty and contains only lowercase letters a-z.
+* p could be empty and contains only lowercase letters a-z, and characters like . or *.
+* Example 1:
+
+* Input:
+* s = "aa"
+* p = "a"
+* Output: false
+* Explanation: "a" does not match the entire string "aa".
+* Example 2:
+
+* Input:
+* s = "aa"
+* p = "a*"
+* Output: true
+* Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+* Example 3:
+
+* Input:
+* s = "ab"
+* p = ".*"
+* Output: true
+* Explanation: ".*" means "zero or more (*) of any character (.)".
+* Example 4:
+
+* Input:
+* s = "aab"
+* p = "c*a*b"
+* Output: true
+* Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+* Example 5:
+
+* Input:
+* s = "mississippi"
+* p = "mis*is*p*."
+* Output: false          
+*     
 **********************************************************************************/
+
+
+// 220 ms, faster than 12.84% of C++ 15.4 MB, less than 10.17% of C++
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty())    return s.empty();
+        
+        if ('*' == p[1])
+            // x* matches empty string or at least one character: x* -> xx*
+            // *s is to ensure s is non-empty
+            //   0 tims || 1 times
+            return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        else
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
+    }
+};
+
+// https://leetcode.com/problems/regular-expression-matching/discuss/5665/My-concise-recursive-and-DP-solutions-with-full-explanation-in-C%2B%2B
+// 8 ms, faster than 66.41% of C++, 9 MB, less than 66.10% of C++ 
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+        
+        dp[0][0] = true;
+        for(int i=1;i<=m;i++){
+            dp[i][0] = false;
+        }
+        for(int j=1;j<=n;j++){
+            dp[0][j] = j>1 && '*'==p[j-1] && dp[0][j-2];
+        }
+        for (int i = 1; i <= m; i++){
+            for (int j = 1; j <= n; j++){
+                if(p[j-1] != '*'){
+                    dp[i][j] = dp[i-1][j-1] && (s[i-1] == p[j-1] || '.' == p[j-1]);
+                }
+                else{
+                    dp[i][j] = dp[i][j-2] || (s[i-1] == p[j-2] || '.' == p[j-2]) && dp[i-1][j];
+                }
+            }
+        }
+            
+        return dp[m][n];
+        
+        
+    }
+};
+
+
+
+
+
 
 #include <stdio.h>
 #include <string.h>
